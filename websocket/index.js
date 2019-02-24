8ffb0f59ed80652d21b3e46b2c6c591f04c9469c
Geovane Feitosa Cavalcante <geovanefeitosacavalcannte@gmail.com>
2019-02-24 16:04:52 -0300

    first commit

---
+++ websocket/index.js
@@ -0,0 +1,19 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+
+});
